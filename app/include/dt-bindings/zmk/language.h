7dcb947f23fb63ec1535f834ae8fe6c4be12b19d
roma <gemoglobinvnorme@gmail.com>
2025-11-02 19:50:32 +0300

    added &lang_kp and &lang_ch behaviors

---
+++ app/include/dt-bindings/zmk/language.h
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2020 The ZMK Contributors
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define LANG_EN 0
+#define LANG_RU 1
---
+++ app/src/behaviors/behavior_language_key_press.c
@@ -0,0 +1,61 @@
+#define DT_DRV_COMPAT zmk_behavior_language_key_press
+
+#include <zephyr/device.h>
+#include <drivers/behavior.h>
+#include <zmk/behavior.h>
+#include <zmk/event_manager.h>
+#include <zmk/keymap.h>
+#include <zephyr/logging/log.h>
+#include <dt-bindings/zmk/language.h>
+
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+
+extern int global_language;
+
+static int lkp_press(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event)
+{
+    uint32_t keycode_en = binding->param1;
+    uint32_t keycode_ru = binding->param2;
+    uint32_t keycode = binding->param1;
+    LOG_DBG("DM press: keycode=%d", keycode);
+
+    if (global_language == LANG_RU) {
+        keycode = keycode_ru;
+    }
+
+    struct zmk_behavior_binding kp_binding = {
+        .behavior_dev = DEVICE_DT_NAME(DT_NODELABEL(kp)),
+        .param1 = keycode,
+    };
+
+    return zmk_behavior_invoke_binding(&kp_binding, event, true);
+}
+
+static int lkp_release(struct zmk_behavior_binding *binding, struct zmk_behavior_binding_event event)
+{
+    uint32_t keycode_en = binding->param1;
+    uint32_t keycode_ru = binding->param2;
+    uint32_t keycode = binding->param1;
+    LOG_DBG("DM press: keycode=%d", keycode);
+
+    if (global_language == LANG_RU) {
+        keycode = keycode_ru;
+    }
+
+    struct zmk_behavior_binding kp_binding = {
+        .behavior_dev = DEVICE_DT_NAME(DT_NODELABEL(kp)),
+        .param1 = keycode,
+    };
+
+    return zmk_behavior_invoke_binding(&kp_binding, event, false);
+}
+
+static const struct behavior_driver_api lkp_driver_api = {
+    .binding_pressed = lkp_press,
+    .binding_released = lkp_release,
+};
+
+BEHAVIOR_DT_INST_DEFINE(0, NULL, NULL,
+                        NULL, NULL,
+                        APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                        &lkp_driver_api);
